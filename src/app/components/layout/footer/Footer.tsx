f2a5739f59735db6136519c8ed146e1a1104e6df
Odhran <russell.odhran@gmail.com>
2025-01-25 12:54:48 +0000

    add nav and footer

--- src/app/components/layout/footer/Footer.tsx
+++ src/app/components/layout/footer/Footer.tsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import { CiHeart } from "react-icons/ci";
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer className='w-full px-3 border-t-2 border-matchaGreen'>
+        <div className='flex text-matchaGreen tracking-wider items-center justify-between'>
+            <p className='flex'>Made with <CiHeart size={20} color='#90EE90' /> by Sip&Serve.</p>
+            <p className='cursor-pointer'>Submit a rating!</p>
+        </div>
+    </footer>
   )
 }
 
--- src/app/components/layout/nav/Navbar.tsx
+++ src/app/components/layout/nav/Navbar.tsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import { SiCoffeescript } from "react-icons/si"
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <nav className='flex justify-between items-center px-3 pb-3 w-full border-b-2 border-matchaGreen tracking-wider'>
+        <div className='flex space-x-3 items-center cursor-pointer'>
+            <SiCoffeescript color='#90EE90' size={55} />
+            <p className='font-bold text-2xl text-matchaGreen'>Matcha Finder</p>
+        </div>
+        <div className='font-semibold text-l text-matchaGreen'>
+            The best matcha in Ireland.
+        </div>
+    </nav>
   )
 }
 
